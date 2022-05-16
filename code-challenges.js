7a5f97c652b1cf3ee374ff9bc42a89792f63a7d8
NicholasLoiacono <NTLoiacono4@gmail.com>
2022-05-16 09:49:35 -0700

    completed week 1 assessment

--- code-challenges.js
+++ code-challenges.js
@@ -14,6 +14,31 @@ const fruit2 = "banana"
 const fruit3 = "cherry"
 const fruit4 = "kiwi"
 
+// Function Signature
+// input: words
+// output: string indicating the word with the most characters
+
+// Example
+// input: "apple", "banana" // output --> "banana"
+// input: "cherry", "kiwi" // output --> "cherry"
+
+// Process
+// create a function that will take in two words and count the characters in each word and only return the word with the most characters
+// two fruits were named in the beginning of the function
+// next the lengths of each fruit word are established by using .length
+// we asked for a return if the first fruit name was longer than the second and if not then return the name of the second fruit
+
+const longerName = (fruit1, fruit2) => {
+let fruit1length = fruit1.length
+let fruit2length = fruit2.length
+if(fruit1length > fruit2length) {
+    return fruit1
+} else {
+    return fruit2
+}
+}
+console.log(longerName("apple", "banana"))
+console.log(longerName("cherry", "kiwi"))
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
@@ -21,21 +46,131 @@ const temp1 = 42
 const temp2 = 350
 const temp3 = 212
 
+// Function Signature
+// input: numbers
+// output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+
+// Example
+// input: 42 // output --> "42 is below boiling point"
+// input: 350 // output --> "350 is above boiling point"
+// input: 212 // output --> "212 is at boiling point"
+
+// Process
+// 
+
+const boilingPoint = (temp1, temp2, temp3) => {
+if(temp1 < 212) {
+    console.log("42 is below boiling point")
+} else if(temp2 > 212) {
+    console.log("350 is above boiling point")
+} else if(temp3 = 212) {
+    console.log("212 is at boiling point")
+}
+}
+console.log(boilingPoint(42))
+console.log(boilingPoint(350))
+console.log(boilingPoint(212))
+
+// console.log(temp1 + " 42 is below boiling point " + temp3)
+// console.log(temp2 + " 350 is above boiling point " + temp3)
+// console.log(temp3 + " is less than " + temp3)
+
+// const boilingPoint = (temp1, temp2, temp3) => {
+    // let temp1 = 42
+    // let temp2 = 350
+    // let temp3 = 212
+    // if(temp2 > temp3) {
+    //     return nice
+    // } else {
+    //     return nope
+    // }
+    // }
+    // console.log(boilingPoint("42"))
+    // console.log(boilingPoint("350"))
+// const cooking = (temp1, temp2, temp3) => {
+//     let temp1 = 42
+//     let temp2 = 350
+//     let temp3 = 212
+//     if(temp1 < 212) {
+//         return "42 is below boiling point"
+//     } else if(temp2 > 212) {
+//         return "350 is above boiling point"
+//     } else {
+//         return "212 is at boiling point"
+//     }
+// }
+// console.log(cooking(42))
+// console.log(cooking(350))
+// console.log(cooking(212))
+
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+// Function Signature
+// input: arrays
+// output: combined arrays and add up number slots
+
+// Example
+// input: [3, 7, 0, 36, -9] and [8, -7, 42, 9, 13] // output --> [3, 7, 0, 36, -9, 8, -7, 42, 9, 13] 10
+
+// Process
+// create a function that will combine myNumbers1 and myNumbers2
+// next we will call that new function with the comined arrays forward
+// then use .length to find out how many numbers are in the combined array we created
+
+const comboArrays = myNumbers1.concat(myNumbers2)
+console.log(comboArrays)
+console.log(comboArrays.length)
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 eilrahC"
 
 const currentCohort = "Charlie 2022"
 
+// Function Signature
+// input: string of a phrase
+// output: "2202 eilrahC"
+
+// Example
+// input: "Charlie 2022" // output --> "2202 eilrahC"
+
+// Process
+// create an if statement declaring that currentCohort specifically equals "Charlie 2022"
+// next create a console.log that call forward "2202 eilrahC" if currentCohort specifically equals "Charlie 2022"
+
+if(currentCohort === "Charlie 2022") {
+console.log("2202 eilrahC")
+}
+
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
 const myArray = [13, 34, 5, 10, 27, 42]
+
+// Function Signature
+// input: array of numbers
+// output: my function should tell me which numbers are 'odd' and which numbers are 'even'
+
+// Example
+// input: [13, 34, 5, 10, 27, 42] // output --> odd, even, odd, even, odd, even
+
+// Process
+// create a for loop to check each number and whether it is odd or even
+// the index will start at position 0 which is also the position of the number 13
+// my if statement says that if any number [starting from my index point] is divisable by 2 with no remainder (or 0 remainder) then console.log it as "even"
+// my else statement is going to call everything else (after it filters through my if statement) will be called "odd"
+
+for(let index = 0; index < myArray.length; index++){
+
+  if( myArray[index] % 2 === 0){
+    console.log("even")
+  } else {
+    console.log("odd")
+  }
+
+}
 
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
@@ -47,3 +182,19 @@ const number2 = 100
 // Set two:
 const number3 = 27
 const number4 = 24
+
+// Function Signature
+// input: larger numbers and smaller numbers
+// output: smaller numbers being subtracted from larger numbers
+
+// Example
+// input: 100-58 // output --> 42
+// input: 27-24 // output --> 3
+
+// Process
+// create a console.log that will subtract the smaller number from the larger number
+// the first console log will take number2 (100) and subtract number1 (58) from it to get the outcome of 42
+// the second console log will take number3 (27) and subtract number4 (24) from it to get the outcome of 3
+
+console.log(number2-number1)
+console.log(number3-number4)
--- coding-concepts.js
+++ coding-concepts.js
@@ -9,51 +9,51 @@
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will put 'indigo' after 'daffodil'
+// b) Verify and explain: the .push add a value to the end of an array, thus pushing 'indigo' into the end of the array
 
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will give you the length in how many positions there are in the string
+// b) Verify and explain: .length is an informational command that returns the number of elements in the array. The length is the always the last index of the array plus one. This is why even though there are 9 positions (starting from 0) the output is 10
 
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will return the letter 'o'
+// b) Verify and explain: the greeting is being called by the console.log and the [4] is calling the letter in the 4th position in the string. Since we start at position '0' the letter that was returned was 'o'
 
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: it will return the word "Ruby"
+// b) Verify and explain: the const index = 1 is referring to the first position in the array of const languages. So when the console.log calls forward languages[index] the output will read the 1st index of the languages array. With our position starting at '0' the 1st position word is considered 'Ruby'
 
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will make all letters in the words "saturday" and "sunday" upper case (or capitalized)
+// b) Verify and explain: the console log shown above is not for an array, also if it were in parentheses instead the only word to be capitalized would be "sunday"
 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I believe this will return the length of each word in a string along with the positions they are in inside the array
+// b) Verify and explain: the reference website from github says that if number is in the array then number will be returned
